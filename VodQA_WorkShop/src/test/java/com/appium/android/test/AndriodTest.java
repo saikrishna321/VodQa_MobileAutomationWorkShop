cf78a303b94998cab1b6fe907b522e3a49f5af0b
saikrisv <saikrisv@10.4.21.124>
2015-08-05 22:23:08 +0530

    Added WebTest and Added Scroll Functions to Run on Wordpress app

--- VodQA_WorkShop/src/test/java/com/appium/android/test/AndriodTest.java
+++ VodQA_WorkShop/src/test/java/com/appium/android/test/AndriodTest.java
@@ -2,22 +2,30 @@ package com.appium.android.test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.SwipeElementDirection;
 import io.appium.java_client.android.AndroidDriver;
 
 public class AndriodTest {
 
+	private static final String Image_Scrollable = "org.wordpress.android:id/image_featured";
+	public static final String PASSWORD = "org.wordpress.android:id/nux_password";
+	public static final String USERNAME = "org.wordpress.android:id/nux_username";
 	AppiumDriver<WebElement> driver;
 
 	@Before
@@ -25,7 +33,7 @@ public class AndriodTest {
 		DesiredCapabilities caps = new DesiredCapabilities();
 		caps.setCapability("deviceName", "9111833b");
 		caps.setCapability("platformVersion", "5.0.2");
-		caps.setCapability("app", "/Users/saikrisv/Downloads/org.wordpress.android-4.3.2-APK4Fun.com.apk");
+		caps.setCapability("app", System.getProperty("user.dir") + "/build/wordpress.apk");
 		caps.setCapability("package", "org.wordpress.android");
 		caps.setCapability("appActivity", "org.wordpress.android.ui.WPLaunchActivity");
 		driver = new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
@@ -37,27 +45,37 @@ public class AndriodTest {
 	}
 
 	@Test
-	public void testApp() throws InterruptedException{
-		waitForElementClickable(By.id("org.wordpress.android:id/nux_username"),10);
-	    driver(By.id("org.wordpress.android:id/nux_username")).sendKeys("vodqa@gmail.com");
-		driver(By.id("org.wordpress.android:id/nux_password")).sendKeys("Hello12345678");
+	public void testLogin_Swipe_Scroll() throws InterruptedException {
+
+		waitForElementClickable(By.id(USERNAME), 10);
+		driver(By.id(USERNAME)).sendKeys("vodqa@gmail.com");
+		driver(By.id(PASSWORD)).sendKeys("Hello12345678");
 		driver(By.name("Sign in")).click();
-		Thread.sleep(5000);
-		Dimension size = driver.manage().window().getSize(); 
-		int  startx= (int) (size.width * 0.9); 
-		int endx= (int) (size.width * 0.20); 
-		int starty = size.height / 2; 
+		waitForElementClickable(By.id("org.wordpress.android:id/switch_site"), 30);
+		Dimension size = driver.manage().window().getSize();
+		int startx = (int) (size.width * 0.9);
+		int endx = (int) (size.width * 0.20);
+		int starty = size.height / 2;
 		driver.swipe(startx, starty, endx, starty, 1000);
+		waitForElementClickable(By.id(Image_Scrollable), 10);
+		scrollDirection(Image_Scrollable,SwipeElementDirection.UP);
+		Thread.sleep(3000);
 	}
 
 	public WebElement driver(By by) {
 		return driver.findElement(by);
 
 	}
-    
-	public void waitForElementClickable(By by,int waitTime){
-	WebDriverWait wait = new WebDriverWait(driver, waitTime);
-    wait.until(ExpectedConditions.elementToBeClickable(by));
-	
-}
+
+	public void waitForElementClickable(By by, int waitTime) {
+		WebDriverWait wait = new WebDriverWait(driver, waitTime);
+		wait.until(ExpectedConditions.elementToBeClickable(by));
+
+	}
+
+	public void scrollDirection(String Id,SwipeElementDirection arg) {
+		MobileElement e = (MobileElement) driver.findElementById(Id);
+		e.swipe(arg, 1000);
+	}
+
 }
---
+++ VodQA_WorkShop/src/test/java/com/appium/android/test/AndroidWebTest.java
@@ -0,0 +1,53 @@
+package com.appium.android.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+
+public class AndroidWebTest {
+	AppiumDriver<WebElement> driver;
+	@Before
+	public void setUp() throws Exception {
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("deviceName", "9111833b");
+		capabilities.setCapability("platformVersion", "5.0.1");
+		capabilities.setCapability("browserName", "Browser");
+		driver=new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+	}
+	
+	@After
+	public void tearDown(){
+		driver.quit();
+	}
+
+	@Test
+	public void runWebTest() throws Exception {
+		driver.get("http://www.thoughtworks.com");
+		WebElement idElement = driver.findElement(By.id("mobile-menu-title"));
+		assertNotNull(idElement);
+		idElement.click();
+		WebDriverWait wait_1 = new WebDriverWait(driver, 30);
+		wait_1.until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//*[@id='mobile-menu']/li[6]/a")));
+		WebElement commentElement = driver.findElement(By.xpath(".//*[@id='mobile-menu']/li[6]/a"));
+		assertNotNull(commentElement);
+		commentElement.click();
+		Thread.sleep(5000);
+		WebElement contact_us = driver.findElement(By.xpath(".//*[@id='footer-menu']/div/ul/li[9]/a"));
+		contact_us.click();
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//*[@id='contact-us-vertical']")));
+		Thread.sleep(2000);
+	}
+}
